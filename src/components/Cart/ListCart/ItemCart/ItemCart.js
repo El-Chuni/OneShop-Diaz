7382e307d74d756883bbc013a3dcab7c967e1edb
El Chuni <hernandiazarro@hotmail.com>
2022-07-14 23:05:49 -0300

    cartContext arreglado pero falta saber como hacer que sacarDelCarrito funcione, pues no se actualiza (al parecer debería usar useEffect para que actualice pero si no va como creo pues adivinara porque estará leyendo esto)

--- src/components/Cart/ListCart/ItemCart/ItemCart.js
+++ src/components/Cart/ListCart/ItemCart/ItemCart.js
@@ -4,21 +4,21 @@ import { carrito } from "../../../../context/cartContext";
 import close from "../../../media/close.png";
 
 //Se muestra el objeto en el carrito
-const ItemCart = (enCarrito) => {
+const ItemCart = ({producto}) => {
 
     const {sacarDelCarrito} = useContext(carrito)
 
     return (
         
         <div className="d-flex flex-row justify-content-around align-items-center enCarrito p-2 m-2">
-            <img src={enCarrito.img} alt="imagen-en-carrito" />
-            <p className="align-middle font-weight-bold">{enCarrito.producto}</p>
+            <img src={producto.img} alt="imagen-producto" />
+            <p className="align-middle font-weight-bold">{producto.producto}</p>
             <div className="d-flex flex-column justify-content-around align-items-center enCarrito p-2 m-2">
-                <p className="align-middle">Llevando {enCarrito.cantidad}</p>
+                <p className="align-middle">Llevando {producto.cantidad}</p>
                 <p className="align-middle">Total:</p>
-                <p className="align-middle">ARG$ {enCarrito.precio}</p>
+                <p className="align-middle">ARG$ {producto.precio}</p>
             </div>
-            <img src={close} alt="close" className="quitar" onClick={sacarDelCarrito}></img>
+            <button onClick={sacarDelCarrito}><img src={close} alt="close"/></button>
         </div>
     )
 }
